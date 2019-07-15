705613658d0d895f3da7e57db4f0376bef0cd53f
Todd McKenzie <39514979+toddmckenzie@users.noreply.github.com>
2019-07-15 15:46:17 -0500

    mobile styling added to auth page (#8)

--- src/routes/AuthenticatePage.js
+++ src/routes/AuthenticatePage.js
@@ -7,21 +7,40 @@ import {
     Redirect,
     withRouter
   } from "react-router-dom";
+  import { Button, Box, Grid } from '@material-ui/core';
+  import '../scss/authenticationPage.scss';
+  // import Box from '@material-ui/core/Box';
 
-class AuthenticatePage extends React.Component
-{
-  //  authenticate = (e) => {
-  //    console.log(e);
-  //   const authType = e.target.name;
-  //   this.props.getUserData()
-    
-  //   axios.get("https://cloudstands-staging.herokuapp.com/auth/google").then(console.log("did it"))
-
-  // }
-
-  // this.props.getUserData()
+class AuthenticatePage extends React.Component {
     render()
     {
+
+ AuthenticationStyling
+        return (
+        <Grid container
+        direction="column"
+        justify="center"
+        alignItems="center" >
+          <h1>Cloud Stands</h1>
+          <Box className="header-SignIn" display='flex'>
+            
+              <span className="meet-the-team"><p>Meet the Team</p></span>
+            
+                <p>Sign Up</p>
+                <Button variant="outlined" color="white" label="Google" onClick={this.authenticate} style={{ background: "white", Color: "white", textDecoration: "none" }} >Sign In</Button>
+                
+          </Box>
+          
+          <Box className='buttons' display='flex' flexDirection='column'>
+            <h1>Create Account</h1>
+            <Button variant="contained" color="white" label="Google" onClick={this.authenticate} style={{ textDecoration: "none", marginTop: "5px", borderRadius: "10px",  height: "50px", color: "black", backgroundColor: "white", border: "1px solid black" }} >Sign Up with Square</Button>
+              <a href="https://cloudstands-staging.herokuapp.com/auth/google"><Button variant="contained" color="white" label="Google" style={{ background: " #d3d3d3", textDecoration: "none", height: "30px", marginTop: "15px", borderRadius: "10px", height: "50px" }} >Sign Up with Google</Button></a>
+              <Button variant="contained" color="white" label="Google" onClick={this.authenticate} style={{ background: "#3b5998", color: "white", textDecoration: "none", marginTop: "15px", borderRadius: "10px",  height: "50px"  }} >Sign Up With Facebook</Button>
+              <p>By continuing, you agree to Cloud Stands Terms of Service and Privacy Policy</p>
+              <p>Already have an account? Sign in</p>
+           </Box>
+
+        </Grid>)
 
         return (<div>
           {/* <button onClick={this.authenticate} name="google">Google</button>
@@ -32,6 +51,7 @@ class AuthenticatePage extends React.Component
           </a>
           {/* </Link> */}
         </div>)
+ AuthenticationPage
     }
 
     
