dd7631c20ab95f24a3e06c9de0e1fea0953f30ca
AI2EJAY <39928154+AI2EJAY@users.noreply.github.com>
2019-07-16 17:14:01 -0500

    Main page pushed

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -41,4 +41,47 @@ class LandingPage extends React.Component
     }
   )(App); */
   
+export default LandingPage;import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect,
+    withRouter
+  } from "react-router-dom";
+
+class LandingPage extends React.Component
+{
+    render()
+    {
+        return(
+        <div>
+          <div>Welcome Market Owner</div>
+          <button onClick={()=> this.logout()}>Logout</button>
+        </div>
+        )
+    }
+    logout()
+    {
+        localStorage.clear();
+        this.props.history.push("/");
+    }
+    
+}
+
+/* const mapStateToProps = state => {
+    return {
+      //states
+      ...state.checkUserData
+    };
+  };
+  
+  export default connect(
+    mapStateToProps,
+    { //actions
+      setLocalData
+     // getUserData 
+    }
+  )(App); */
+  
 export default LandingPage;
