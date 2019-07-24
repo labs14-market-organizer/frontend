96dda6c31f814b5fb5d514e08f42be8f94f66562
AI2EJAY <39928154+AI2EJAY@users.noreply.github.com>
2019-07-24 14:15:50 -0500

    updated nav bar some. Not finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,67 +8,60 @@ import {
   Container,
   CssBaseline
 } from "@material-ui/core";
-import cloud from "../assets/topCloud.svg";
+import cloud from "../assets/cloud.svg";
 import { ThemeProvider } from "@material-ui/styles";
 import MenuIcon from "@material-ui/icons/Menu";
 import { createMuiTheme } from "@material-ui/core/styles";
+import styled from "styled-components";
 
-const cloudText = createMuiTheme({
-  typography: {
-    h3: {
-      fontFamily: ["Luckiest Guy"]
-    }
-  },
-  palette: {
-    primary: {
-      main: "#7f817e"
-    }
-  },
-  button: {
-    color: "#7f817e"
-  }
-});
+const CloudText = styled(({ variant, ...otherProps}) => <Typography variant="h3" {...otherProps} />)`
+    font-family: "Luckiest Guy";
+    color: #7f817e;
+`;
 
-const standsText = createMuiTheme({
-  typography: {
-    h3: {
-      fontFamily: ["Raleway Dots"]
-    }
-  },
-  palette: {
-    primary: {
-      main: "#000000"
-    }
-  }
-});
+const StandsText = styled(({ variant, ...otherProps}) => <Typography variant="h3" {...otherProps} />)`
+    font-family: "Raleway Dots";
+    color: black;
+`;
+
+const StyledBox = styled(({ m, p, ...otherProps}) => <Box m={0} p={0} {...otherProps} />)`
+
+img {
+}
+`;
+
+
+
+
+
 
 const Navbar = () => {
   return (
-    <Box m={0} p={0}>
-    <Container>
-        <img src={cloud} boxShadow={1} zIndex="bottom" width="100%" height="100%" />
-        <Grid container justify="space-between">
-          <ThemeProvider theme={cloudText}>
-            <Box mt={-14}>
-              <Typography color="primary" variant="h3">
-                CLOUD
-              </Typography>
-            </Box>
-
-            <ThemeProvider theme={standsText}>
+    <StyledBox m={0} p={0}>
+        <img src={cloud} zIndex="bottom" width="100%" height="100%" />
+      <Container>
+        <Grid container justify="space-around">
+          <Box mt={-13}>
+            <MenuIcon />
+          </Box>
+            <Grid item xs={3}>
               <Box mt={-14}>
-                <Typography color="primary" variant="h3">
-                  STANDS
-                </Typography>
+        
+                <CloudText>
+                  CLOUD
+                </CloudText>
+
               </Box>
-              <Box mt={-13}>
-                <MenuIcon />
-              </Box>
-            </ThemeProvider>
-          </ThemeProvider>
+            </Grid>
+
+            <Box mt={-14}>
+              <StandsText>
+                STANDS
+              </StandsText>
+            </Box>
         </Grid>
       </Container>
-      </Box>
+    </StyledBox>
   );
 };
 
