29c2186b844f5d2eb41f602eb8dd86bcfe0808be
DwayneDevelops <DwayneDevelops@gmail.com>
2019-07-26 12:00:29 -0500

    created `Booth Type` component

--- src/routes/AddBooths.js
+++ src/routes/AddBooths.js
@@ -1,17 +1,45 @@
 import React from "react";
 import Arrow from "../assets/ic-arrow-back.svg";
-import { bgWhite } from "ansi-colors";
-import color from "@material-ui/core/colors/lightGreen";
+import { TextField } from '@material-ui/core';
 
 class AddBooths extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            boothtype: ''
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+          ...this.state,
+          [e.target.name]: e.target.value
+        });
+      };
+
+
     render() {
         return (
             <form>
                 <div className="header">
                     <img src={Arrow} />
                     <h4 className="addbooths">Add Booths</h4>
+                </div><br />
+                <div>
+                    <TextField 
+                        required
+                        id="boothtype"
+                        label="Booth Type"
+                        name="boothtype"
+                        value={this.state.boothtype}
+                        onChange={this.handleChange}
+                        margin="normal"
+                        variant="outlined"
+                        fullWidth={true}
+                        autoComplete={true}
+                    />
+
                 </div>
-                
             </form>
         )
     }
