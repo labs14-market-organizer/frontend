54a6bcd908f1403e98e8abd2a931df47412ef1f0
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-07-26 10:46:13 -0500

    changes to save button

--- src/routes/CreateMarket.js
+++ src/routes/CreateMarket.js
@@ -110,68 +110,71 @@ class CreateMarket extends React.Component {
 
   setHours = e => {
     e.preventDefault();
-    let startTime = this.state.start;
-    let endTime = this.state.end;
-    let newDaysList = this.state.daysList;
-    let currentOpList = this.state.operation;
-    const normalWeek = [
-      "sunday",
-      "monday",
-      "tuesday",
-      "wednesday",
-      "thursday",
-      "friday",
-      "saturday"
-    ];
-    let opList = [];
-    for (let i = 0; i < currentOpList.length; i++) {
-      //the purpose of these loops are to check to see if the old list has same days as the new list and if it does the old list day will be removed.
-      for (let x = 0; x < newDaysList.length; x++) {
-        if (currentOpList[i] !== undefined) {
-          if (currentOpList[i].day === newDaysList[x][0]) {
-            currentOpList.splice(i, 1);
-            i--;
+    if (this.state.start < this.state.end) {
+      let startTime = this.state.start;
+      let endTime = this.state.end;
+      let newDaysList = this.state.daysList;
+      let currentOpList = this.state.operation;
+      const normalWeek = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday"
+      ];
+      let opList = [];
+      for (let i = 0; i < currentOpList.length; i++) {
+        //the purpose of these loops are to check to see if the old list has same days as the new list and if it does the old list day will be removed.
+        for (let x = 0; x < newDaysList.length; x++) {
+          if (currentOpList[i] !== undefined) {
+            if (currentOpList[i].day === newDaysList[x][0]) {
+              currentOpList.splice(i, 1);
+              i--;
+            }
           }
         }
       }
-    }
 
-    for (let i = 0; i < newDaysList.length; i++) {
-      //new market hour days are added.
-      opList.push({
-        day: newDaysList[i][0],
-        start: startTime,
-        end: endTime
-      });
-    }
+      for (let i = 0; i < newDaysList.length; i++) {
+        //new market hour days are added.
+        opList.push({
+          day: newDaysList[i][0],
+          start: startTime,
+          end: endTime
+        });
+      }
 
-    let sortOperation = []; //sorting the days.
-    let op = [...currentOpList, ...opList];
-    for (let i = 0; i < normalWeek.length; i++) {
-      for (let x = 0; x < op.length; x++) {
-        if (normalWeek[i] === op[x].day) {
-          sortOperation.push(op[x]);
+      let sortOperation = []; //sorting the days.
+      let op = [...currentOpList, ...opList];
+      for (let i = 0; i < normalWeek.length; i++) {
+        for (let x = 0; x < op.length; x++) {
+          if (normalWeek[i] === op[x].day) {
+            sortOperation.push(op[x]);
+          }
         }
       }
+
+      //currently its hard coded.
+      this.setState({
+        ...this.state,
+        // operation: [...currentOpList, ...opList],
+        operation: [...sortOperation],
+        daysHoursField: this.state.daysHoursField + 1,
+        daysList: [],
+        monday: false,
+        tuesday: false,
+        wednesday: false,
+        thursday: false,
+        friday: false,
+        saturday: false,
+        sunday: false
+      });
+    } else {
+      alert('Please change the time to be accurate')
     }
-
-    //currently its hard coded.
-    this.setState({
-      ...this.state,
-      // operation: [...currentOpList, ...opList],
-      operation: [...sortOperation],
-      daysHoursField: this.state.daysHoursField + 1,
-      daysList: [],
-      monday: false,
-      tuesday: false,
-      wednesday: false,
-      thursday: false,
-      friday: false,
-      saturday: false,
-      sunday: false
-    });
-  };
-
+};
   save = e => {
     e.preventDefault();
     if (this.state.isUpdating) {
@@ -189,7 +192,7 @@ class CreateMarket extends React.Component {
       <form>
         <div className="header">
           <img src={Arrow} />
-          <h4 className="createHeader">Create Market</h4>
+          <h4 className="createHeader">{(this.isUpdating) ? "Edit Market" : "Create Market" }</h4>
         </div>
         <div className="addPhoto">
           <img />
@@ -334,7 +337,7 @@ class CreateMarket extends React.Component {
           onChange={e => this.onRadioChange(e)}
         />
         Private Market
-        <h6>Select market hours of operation</h6>
+        <h4>Market Days {'&'} Times Of Operation</h4>
         <div>
           <StyledDiv>
             <Button
@@ -433,7 +436,7 @@ class CreateMarket extends React.Component {
             +ADD HOURS
           </Button>
         </StyledDiv>
-        <br />
+        
         
       
        
@@ -450,7 +453,7 @@ class CreateMarket extends React.Component {
             color="secondary"
             onClick={e => this.save(e)}
           >
-            SAVE
+            NEXT
           </Button>
         </SaveFix>
       </form>
