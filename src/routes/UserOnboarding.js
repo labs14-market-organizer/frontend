18f626fddb41ff9961a0d2ed847449497d6e8506
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-07-26 12:30:25 -0500

    styled test

--- src/routes/UserOnboarding.js
+++ src/routes/UserOnboarding.js
@@ -2,24 +2,35 @@ import React from "react";
 import { Box, Shadows, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
+import styled from "styled-components";
+
 const UserOnboarding = () => {
     return (
-        <>
+        <div>
         <Typography>
             Cloud stands wants to provide you with the best possible experience.  Which best describes you roles?
         </Typography>
         <Link to="/createmarket" style={{textDecoration: "none"}} >
-            <Box boxShadow={10} style={{ height: '40vw', width: '90vw', marginLeft: '5%', marginBottom: "20px", textAlign: "center", borderRadius: "10px" }}>
-                <Typography > Market Owner </Typography>
-            </Box>
+                <StyleBox boxShadow={10} >
+                    <Typography > Market Owner </Typography>
+                </StyleBox>
         </Link>
         <Link to="" style={{textDecoration: "none"}}>
             <Box boxShadow={10} style={{ height: '250px'}}>
                 Vendor
            </Box>
         </Link>
-        </>
+        </div>
     )
 }
+
+const StyleBox = styled(Box)`
+        height: '40vw';
+        width: '90vw'; 
+        marginLeft: '5%'; 
+        marginBottom: "20px"; 
+        textAlign: "center"; 
+        borderRadius: "10px";
+`;
 
 export default UserOnboarding;
