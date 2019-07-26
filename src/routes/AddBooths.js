bc3c322af01b3b02a4991936e33475b7a95e4fad
DwayneDevelops <DwayneDevelops@gmail.com>
2019-07-26 11:39:12 -0500

    added header, will add styling later

--- src/routes/AddBooths.js
+++ src/routes/AddBooths.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Arrow from "../assets/ic-arrow-back.svg";
+import { bgWhite } from "ansi-colors";
+import color from "@material-ui/core/colors/lightGreen";
 
 class AddBooths extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Hello World!</h1>
-            </div>
+            <form>
+                <div className="header">
+                    <img src={Arrow} />
+                    <h4 className="addbooths">Add Booths</h4>
+                </div>
+                
+            </form>
         )
     }
 }
