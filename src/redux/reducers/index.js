9932b3dce5046b65d6af540b4c421cb1be0551d4
Chase Wex <clapingmanly@hotmail.com>
2019-07-30 16:55:43 -0400

    added forms to the reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import {checkUserData } from "./userData";
 import {checkMarketData}from "./marketData";
+import {reducer as reduxFormReducer} from 'redux-form';
 
 export default combineReducers({
     //list reducers
+    form: reduxFormReducer,
     checkUserData,
     checkMarketData
 });
