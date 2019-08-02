32b2163422524608d6acf95fe86aecf857c0d91a
Chase Wex <clapingmanly@hotmail.com>
2019-08-01 21:09:39 -0400

    Updated functionality and styling

--- src/routes/Booths-Redux-forms.js
+++ src/routes/Booths-Redux-forms.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Arrow from "../assets/ic-arrow-back.svg";
+import Edit from "../assets/border-color-24-px.svg";
 import {
   TextField,
   InputAdornment,
   MuiThemeProvider,
   createMuiTheme,
   Typography,
-  Container
+  Container,
+  Box
 } from "@material-ui/core";
 //import ToggleButton from '@material-ui/lab/ToggleButton';
 import Button from "@material-ui/core/Button";
@@ -22,6 +24,9 @@ import { Field, reduxForm } from "redux-form";
 import {Redirect} from "react-router-dom";
 import FormControlLabel from '@material-ui/core/FormLabel'
 import { maxWidth } from "@material-ui/system";
+import { checkMarketData } from "../redux/reducers/marketData";
+import MarketReduxForms from "./Market-Redux-forms";
+import {getMarketById} from "../redux/actions/marketData"
 
 function validate (values) {
   const errors = {};
@@ -35,11 +40,14 @@ function validate (values) {
         field.slice(1)} Is Required`;
     }
   });
-  console.log(values.width);
-  values.width = values.width < 1 ? "" : parseInt(values.width);
-  values.length = values.length < 1 ? "" : parseInt(values.length);
-/*   console.log("errors: \n" + JSON.stringify(errors));
-  console.log("values:\n" + JSON.stringify(values)); */
+  values.length = parseInt(String(Math.abs(parseInt(values.length))).substring(2,0));
+  values.width = parseInt(String(Math.abs(parseInt(values.width))).substring(2,0));
+  values.numberofbooths = parseInt(String(Math.abs(parseInt(values.numberofbooths))).substring(3,0));
+  //values.width.length = 2;
+  values.boothprice = Math.abs(parseInt(values.boothprice * 100)/100); // cap it to 2 decimal places
+  if(values.boothprice <= 0) values.boothprice = "";
+/*   console.log("errors: \n" + JSON.stringify(errors));*/
+  //console.log(values); 
   return errors;
 }
 
@@ -68,32 +76,81 @@ class CreateMarket extends React.Component
   isUpdating = false;
   redirect = false;
   currentBooths = []
+  currentMarket = null;
+  erase = false;
+  hasUpdated = true;
   constructor(props)
   {
     super(props);
     if(this.props.currentBooth)
-        this.state = this.props.currentBooth;
+      this.state = this.cleanData(this.props.currentBooth);
     else
-    this.state = {
-        boothtype: '',
-        numberofbooths: '',
-        boothprice: '',
-        length: '',
-        width: '',
-        boothdescription: '',
-    }
+      this.state = {
+          id: -1,
+          boothtype: '',
+          numberofbooths: '',
+          boothprice: '',
+          length: '',
+          width: '',
+          boothdescription: '',
+      }
+
     this.isUpdating = !!this.props.currentBooth
       this.props.initialize(
         this.state
       )
+      if(this.props.market)
+      {
+        this.currentMarket = this.props.market;
+        this.currentBooths = this.props.market.booths.map(x=> this.cleanData(x));
+      }
+      else this.redirect = true;
   }
-  
+  cleanData(booth)
+  {
+    return {
+      id: booth.id ? booth.id : -1,
+      boothtype: booth.type,
+      numberofbooths: booth.number,
+      boothprice: booth.price ? booth.price : "",
+      length: booth.size && booth.size.length > 0 && booth.size[0] ? booth.size[0] : "",
+      width: booth.size && booth.size.length > 1 && booth.size[1] ? booth.size[1] : "",
+      boothdescription: booth.description ? booth.description : "",
+    }
+  }
+  changeCurrent(booth, update = true)
+  {
+    
+    this.isUpdating = update;
+    this.currentBooth = booth;
+    this.props.initialize(
+     booth
+    )
+  }
+  componentDidUpdate()
+  {
+     if(this.erase && this.props.valid && this.props.refresh)
+     {
+      this.currentBooth = null;
+      this.erase = false;
+      this.props.initialize(this.cleanData({}))
+     }
+    this.hasUpdated = true;
+    
+  }
+  componentWillUpdate()
+  { 
+    this.currentBooths = this.props.market.booths.map(x=> this.cleanData(x));
+  }
   render(){
     const {handleSubmit, pristine, reset, submitting } = this.props;
-    if(this.props.redirect) if(this.redirect) return <Redirect to="/"/> 
-    else {this.currentBooths.push(this.props.checkBoothData.boothData); console.log(this.currentBooths); reset();}
+    if(this.props.clear && this.erase)
+    {
+      this.erase = false;
+      reset();
+    }
     return (
-      <form onSubmit={handleSubmit}>
+      <div>
         <header 
           className="header"
         >
@@ -108,7 +165,7 @@ class CreateMarket extends React.Component
         
         <body 
           style={{
-              width:'98%',
+              width:'95%',
               height: '100%',
               margin: '10px auto',
               maxWidth: '624px',
@@ -117,13 +174,69 @@ class CreateMarket extends React.Component
               alignItems: "center"
           }}
         >
-          <div style={{display: "flex", flexDirection: "column"}}>
-            {this.currentBooths.map(x=> 
-              <div>
-                Name: {x.boothtype} Number: {x.numberofbooths}
+          {this.isUpdating && !this.currentBooth ? "" : this.form() }
+         <div style={{display: "flex", flexDirection: "column"}}>
+            {this.currentBooths.map(x=> {
+              let flag =  this.props.form.BoothsForm.values && this.props.form.BoothsForm.values.id === x.id ;
+              let obj = flag ? this.props.form.BoothsForm.values : x;
+              return <StyleBox boxShadow={10}>
+              <div className="main-box">
+                <div className="price-text">{ obj.price && obj.price > 0 ? `$${Math.round(obj.price)}` : "free"}</div>
+                <div className="title-box">
+                  <div className="title-text">{obj.boothtype}</div>
+                  <div className="title-subtext">{`${obj.numberofbooths} ${obj.numberofbooths < 2 ? "booth":"booths"}`}</div>
+                </div>
               </div>
+              <div className="edit-box" onClick={()=> {{this.changeCurrent(flag ? null : x);}}} style={flag ? {opacity: "0.5"}: {}} >
+                <img src={Edit}/>
+                <div className="edit-text">edit</div>
+              </div>
+            </StyleBox>
+            }
             )}
           </div>
+            {this.isUpdating ? 
+                <Button 
+                  variant="outlined"
+                  color="secondary"
+                  fullWidth
+                  onClick={() => {this.isUpdating = false; this.changeCurrent(null, false)}}
+                  style={{
+                      
+                      fontSize:'1.4em',
+                      margin: '4% 0',
+                      height: '80px',
+                      width: "65%"
+                  }}
+                >
+                  Add Group
+              </Button>
+              :
+              ""
+            }
+        </body>
+        
+      </div>
+    )
+
+    {/* <Field
+            component={renderTextField}
+            required
+            id="name"
+            label="Market Name"
+            name="Market Name"
+          /> */}
+    
+  }
+  form({handleSubmit, pristine,submitting}= this.props)
+  {
+    return <form onSubmit={handleSubmit}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+              }}
+          >
           <Field
             component={renderTextField}
             required
@@ -148,7 +261,7 @@ class CreateMarket extends React.Component
           <div
             style={{
                 display: 'flex',
-                justifyContent: 'space-evenly'
+                justifyContent: 'space-evenly',
           }}>
             <Field
             component={renderTextField}
@@ -156,6 +269,7 @@ class CreateMarket extends React.Component
             id="numberofbooths"
             label="Number of Booths"
             name="numberofbooths"
+            type="number"
             InputProps={{
               startAdornment: <InputAdornment position="start"></InputAdornment>
             }}
@@ -198,7 +312,7 @@ class CreateMarket extends React.Component
             min="0"
             max="99"
             style={{
-                width: '76px',
+                width: '80px',
                 marginBottom: '20px'
             }}
             InputProps={{
@@ -214,7 +328,7 @@ class CreateMarket extends React.Component
               min="0"
               max="99"
               style={{
-                  width: '76px',
+                  width: '80px',
                   marginBottom: '20px'
               }}
               InputProps={{
@@ -231,49 +345,42 @@ class CreateMarket extends React.Component
             margin="normal"
             style={{ marginBottom: "0px"}}
           />
-          <Button 
-          type="submit" 
-          disabled={pristine || submitting}
-          variant="contained"
-          color="primary"
-          fullWidth
-          onClick={(e)=> { this.redirect = true}}
-          style={{
-              fontSize:'1.75em',
-              margin: '2% auto',
-              height: '80px'
-          }}
-          >
-            Submit
-          </Button>
-        <hr/>
+          <div style={{display: "flex", minWidth: "100%"}} >
         <Button 
           type="submit" 
           disabled={pristine || submitting}
           variant="outlined"
           color="secondary"
           fullWidth
+          onClick={() => {this.props.array.insert("redirecttype",0,1); this.erase = true; }}
           style={{
               
-              fontSize:'1.75em',
-              margin: '2% auto',
-              height: '80px',
+              fontSize:'1.4em',
+              margin: '4% 0',
+              height: '80px'
           }}
         >
-            Submit
+            Add Group
         </Button>
-        </body>
+        <div style={{margin: "0 10px"}}/>
+          <Button 
+          type="submit" 
+          disabled={pristine || submitting}
+          variant="contained"
+          color="primary"
+          fullWidth
+          onClick={(e)=> {this.props.array.insert("redirecttype",0,2);}}
+          style={{
+              fontSize:'1.4em',
+              margin: '4% 0',
+              height: '80px'
+          }}
+          >
+            Save
+          </Button>
+        </div>
+        <hr/>
         </form>
-    )
-
-    {/* <Field
-            component={renderTextField}
-            required
-            id="name"
-            label="Market Name"
-            name="Market Name"
-          /> */}
-    
   }
 };
 
@@ -283,7 +390,86 @@ const mapStateToProps = state => {
   };
 };
 
-
+const StyleBox = styled(Box)`
+ width: 90vw;
+ height: 60px;
+ border-radius: 10px;
+ box-shadow: 0 4px 16px 0 rgba(0, 0, 0, 0.5);
+ background-color: #edf3ea;
+ display: flex;
+ flex-direction: row;
+ justify-content: space-between;
+ margin: 10px 0;
+ @media(min-width: 450px)
+ {
+   width: 450px;
+ }
+ .main-box
+ {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  margin-top: 10px;
+ }
+ .title-box
+  {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+ .price-text
+ {
+  margin: 3px 20px;
+  font-family: Roboto;
+  font-size: 18px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 0.89;
+  letter-spacing: normal;
+  color: #478529;
+ }
+ .title-subtext
+ {
+  margin: 0;
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.33;
+  letter-spacing: normal;
+  color: #484848;
+ }
+ .title-text
+ {
+  margin: 0;
+  font-family: Raleway;
+  font-size: 18px;
+  font-weight: 600;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.33;
+  letter-spacing: normal;
+  color: #000000;
+ 
+ }
+ .edit-box
+ {
+   margin: 10px 10px;
+ }
+ .edit-text
+ {
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: 300;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.33;
+  letter-spacing: normal;
+  color: #000000;
+}
+`;
 
 const ReduxForms = reduxForm({
   form: "BoothsForm", // a unique identifier for this form
@@ -296,20 +482,30 @@ class ReduxContainer extends React.Component
   handleRedux = (values) =>
   {
     this.wasfetching = true;
-    console.log(values);
-    if (this.props.currentBooth) this.props.updateMarket(this.props.currentMarket.id, values)
-    else this.props.createNewBooth({ ...values});
+    this.redirecttype = values.redirecttype && values.redirecttype.length > 0 ? values.redirecttype[values.redirecttype.length-1 ] : 0;
+    if (values.id > 0) this.props.updateBooth(this.props.checkMarketData.marketData.id, values)
+    else this.props.createNewBooth(77,{ ...values});
   }
   wasfetching =false;
+  redirecttype= 0;
   render(){
-    let redirect = this.props.checkBoothData.updated && this.wasfetching;
-    console.log(this.wasfetching);
+    if(!this.props.checkMarketData.marketData) 
+    //temp code so we can test
+    { if(!this.props.checkMarketData.fetching || !this.wasfetching) this.props.getMarketById(77); this.wasfetching = false; return <div>loading</div>}
+    if(this.redirecttype === 2) return <Redirect to="/"/>
+    let clear = this.redirecttype===1;
+    this.redirecttype = 0;
+    return <ReduxForms onSubmit={this.handleRedux} refresh={clear} market={this.props.checkMarketData.marketData} />
+   /*  let redirect = this.props.checkBoothData.updated && this.wasfetching;
     if(this.wasfetching && !this.props.checkBoothData.fetching) this.wasfetching = false;
-    return (<ReduxForms onSubmit={this.handleRedux} redirect={redirect} market={this.props.currentMarket}/>);
+    return (<ReduxForms onSubmit={this.handleRedux} refresh={""} redirect={redirect} market={this.props.checkMarketData.marketData}/>); */
+  }
+  componentDidUpdate()
+  {
   }
 }
 
 export default connect(
   mapStateToProps,
-  { createNewBooth, updateBooth }
+  { createNewBooth, updateBooth, getMarketById }
 )(ReduxContainer);
