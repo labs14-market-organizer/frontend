aa6bfca2e6f13028095d08e5b56aeea05dfdc57e
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-08-07 15:26:32 -0500

    added console.log to vendor

--- src/routes/VendorProfile.js
+++ src/routes/VendorProfile.js
@@ -8,6 +8,7 @@ import { Mixpanel } from '../redux/actions/mixpanel';
 
 const VendorProfile = (props) => {
         let vendor = props.vendorData;
+        console.log(vendor)
         return (
             <div>
                 <Header>
