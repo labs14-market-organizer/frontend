2ad57fd05a6f88f15c0eb6b74dad57e58bc17f4a
Chase Wex <clapingmanly@hotmail.com>
2019-08-13 15:06:35 -0400

    Added styling for clicking on markets and creates popup to view the market

--- src/routes/SearchMarkets.js
+++ src/routes/SearchMarkets.js
@@ -8,62 +8,85 @@ import {
   } from "@material-ui/core";
 import styled from "styled-components";
 import Place from '../assets/place.svg';
-import { searchMarkets } from '../redux/actions/searchMarkets';
+import { searchMarkets, clearSearch } from '../redux/actions/searchMarkets';
+import {localMarketSwitch} from '../redux/actions/marketData'
 import { connect } from "react-redux";
 import Navbar from "../components/Navbar";
 import { Mixpanel } from '../redux/actions/mixpanel';
+import ViewMyMarket from "./ViewMyMarket";
+import { keys } from "@material-ui/core/styles/createBreakpoints";
 
 class SearchMarkets extends React.Component {
     state = {
-        search: ''
+        search: '',
+        popup:  -1
     }
 
     handleChange = e => {
         this.setState({
           [e.target.name]: e.target.value
+
         });
       };
-    
+      handleClear = e => {
+          setTimeout(this.props.clearSearch, 1000);
+          this.setState({...this.state, search:''})
+      }
       startSearch = e => {
           e.preventDefault();
+          if(this.props.marketsBySearch.marketsBySearch) setTimeout(()=>this.props.searchMarkets(this.state.search), 1000);
           if (this.state.search === '') return null;
           Mixpanel.track(`User searched for markets in ${this.state.search}`);
           this.props.searchMarkets(this.state.search);
       }
-
+      handleClick = (e,market) => 
+      {
+        this.props.localMarketSwitch(market);
+        this.setState({...this.state, popup: 1})
+      }
 
     render() {
+        let popup = this.state.popup > 0 && !this.props.market.fetching && this.props.market.marketData;
         return ( 
             <div>
             <Navbar />
             <StyledContainer>
            
-                <form onSubmit={this.startSearch}>
-                    <TextField
-                        id="search"
-                        label="Search Markets"
-                        name="search"
-                        placeholder="City, State or ZIP Code"
-                        value={this.state.search}
-                        onChange={this.handleChange}
-                        margin="normal"
-                        variant="outlined"
-                        fullWidth={true}
-                        style={{marginTop: "60px"}}
-                    />
+                <form onSubmit={this.startSearch} >
+                    <div style={{display: "flex"}}>
+                        <TextField
+                            id="search"
+                            label="Search Markets"
+                            name="search"
+                            placeholder="City, State or ZIP Code"
+                            value={this.state.search}
+                            onChange={this.handleChange}
+                            margin="normal"
+                            variant="outlined"
+                            fullWidth={true}
+                            style={{marginTop: "60px"}}
+                        />
+                        <div style={{margin: "60px -50px", fontSize: "2rem", color: this.state.search !== "" ? "#555": "#AAA", zIndex: 1, cursor: "pointer"}} onClick={this.handleClear}>x</div>
+                    </div>
                 </form>
                 <StyledError>{this.props.searchError ? this.props.searchError : "" }</StyledError>
-                {(this.props.marketsBySearch.marketsBySearch !== undefined) ? this.props.marketsBySearch.marketsBySearch.map(location => {
+                <div id={this.props.marketsBySearch.marketsBySearch && this.state.search !=="" ? "visible" : "invisible"}>
+                {(this.props.marketsBySearch.marketsBySearch !== undefined) ? this.props.marketsBySearch.marketsBySearch.map((location, index) => {
                     return (
-                        <StyleBox boxShadow={10} key={location.id}>
+                        <StyleBox boxShadow={10} key={location.id} name={index} onClick={e => this.handleClick(e, location)}>
                         <p style={{fontWeight: "600"}}>{location.name}</p>
                         <p>{location.description}</p>
                     </StyleBox>
                     )
                     
                 }) : null }
-                
+                </div>
             </StyledContainer>
+            {<Popup>
+                <div id={popup ? "visible" : "invisible"}>
+               { popup ? <ViewMyMarket backcb={()=> {this.setState({...this.state, popup: -1})}} /> : ""}
+               </div>
+            </Popup>}
             </div>
         )
     }
@@ -77,6 +100,16 @@ const StyledContainer = styled(Container)`
     @media(min-width: 600px){
     width: 600px;
     padding-top: 100px;
+  }
+  #visible {
+        opacity: 1.0;
+        height: 100%;
+        transition: opacity 1s linear;
+    }
+  #invisible {
+    opacity: 0.0;
+    height: 0;
+    transition: opacity 1s linear;
   }
 `;
 
@@ -109,15 +142,36 @@ const StyleBox = styled(Box)`
   }
 `;
 
+const Popup = styled.div`
+    z-index: 10000;
+    position: fixed;
+    top: 0px;
+    margin: 0 auto;
+    overflow: scroll;
+    max-height: 100vh;
+    #visible {
+        opacity: 1.0;
+        transition: opacity 0.6s ease-in;
+        background-color: white;
+        min-height: 100vh;
+        width: 100vw;
+    }
+  #invisible {
+    opacity: 0.0;
+    transition: opacity 0.15s ease-out;
+    background-color: #FFFFFF00;
+  }
+`
 
 
 const mapStateToProps = state => {
     return {
-        ...state.checkMarketsByArea
+        ...state.checkMarketsByArea,
+        market: {...state.checkMarketData}
     };
   };
   
   export default connect(
     mapStateToProps,
-    { searchMarkets  }
+    { searchMarkets, clearSearch, localMarketSwitch }
   )(SearchMarkets);
