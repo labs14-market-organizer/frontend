09a45779bbb5d821a8a905ee075da2f6989cc444
Chase Wex <clapingmanly@hotmail.com>
2019-08-16 20:54:32 -0400

    updated with functional redux

--- src/components/BoothPicker.js
+++ src/components/BoothPicker.js
@@ -9,22 +9,31 @@ import {
 import {Button} from '@material-ui/core'
 import Expandor from "./Expandor"
 import styled from "styled-components";
-
-
+import {createReservation, getBoothReservations, deleteBoothReservation} from '../redux/actions/boothReserve'
+import {connect} from 'react-redux';
+import {getUserData} from '../redux/actions/userData'
 
 class BoothPicker extends React.Component 
 {
-
+  lastDate = new Date();
+  disable = false;
   constructor(props)
   {
     super(props);
+    var d = new Date();
+    //look forward a year and see if the market is open
+    for(var i = 0; i < 360; i++) {
+      if(!this.checkHours(d,props.market.operation)) break;
+      d.setDate(d.getDate() + 1);
+    }
     this.state =
     {
-        date: new Date()
+        date: d
     }
   }
   
   handleDateChange(date) {
+    this.props.getBoothReservations(this.props.market.id, formatDate(date))
     this.setState({...this.state, date: date})
   }
   convertObjectToNumberDay(opp)
@@ -34,14 +43,32 @@ class BoothPicker extends React.Component
   }
   checkHours(date,opp)
   {
+    let t = new Date();
+    if(new Date(date.toDateString()) < new Date(new Date().toDateString())) return true;
     let o = this.convertObjectToNumberDay(opp);
     return o.filter(x=> x === date.getDay()).length < 1;
   }
-
+ componentWillMount()
+ {
+   this.props.getBoothReservations(this.props.market.id, formatDate(this.state.date))
+ }
+ componentWillUpdate()
+ {
+   if(!this.props.user.userData && !this.props.user.fetching) this.props.getUserData();
+ }
+ componentDidUpdate()
+ {
+   this.disable = false;
+ }
 render()
 {
     var market = this.props.market;
-   
+    market.booths.map(x=> 
+      {
+        var r = this.props.reserve && !this.props.reserve.fetching && this.props.reserve.reserveData ? this.props.reserve.reserveData.filter(y => x.id === y.id) : null; 
+        x.number = r ? r.length > 0 ? r[0].available : 0 : -1;
+        return x;
+      })
     var price =
     {
         fontFamily: 'Roboto',
@@ -96,75 +123,174 @@ render()
       width: "80vw",
       maxWidth: "500px",
       height: "60px",
-      borderRadius: "ipx",
-      border: "solid 2px rgba(0, 0, 0, 0.1)",
+      fontSize: "18px",
+      fontFamily: "Raleway",
+      fountWeight: "600",
+      borderRadius: "5px",
+      border: "solid 1px rgba(0, 0, 0, 0.1)",
+      letterSpacing: "0.75px",
       backgroundColor: "#478529",
       color: "#FFF",
-      marginTop: "4%"
+      marginTop: "4%",
+      trasition: "color 10s linear"
     }
-
+    var buttonDisabled =
+    {
+      ...button,
+      backgroundColor: "#FFF",
+      borderColor: "#AAA",
+      color: "#AAA"
+    }
+    var buttonRed = 
+    {
+      ...button,
+      backgroundColor: "#FFF",
+      borderColor: "#b21b2d",
+      color: "#b21b2d"
+    }
     return (
         <div>
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
                 <KeyboardDatePicker
-                    style={{zIndex: 1000000000}}
                 disableToolbar
                 shouldDisableDate={(d)=>this.checkHours(d,market.operation)}
                 variant="inline"
                 format="MM/dd/yyyy"
                 margin="normal"
                 id="date-picker-inline"
-                label="Date picker inline"
+                label="Reservation Day"
                 value={this.state.date}
                 onChange={(d) => this.handleDateChange(d)}
+                myDatePickerOptions={{closeAfterSelect: true}}
                 KeyboardButtonProps={{
                     'aria-label': 'change date',
                 }}
                 />
             </MuiPickersUtilsProvider>
             <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+              { this.props.reserve.error ? <ErrorDiv>{ String(this.props.reserve.error)}</ErrorDiv>
+              :
               <Expandor _width="600px">
-                {market.booths.map((x,i,a) => 
-                  <Booth>
-                      <div id="price" style={price}>{`$${Math.round(x.price)}`}</div>
-                      <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
-                        <div style={title}>{x.name}</div>
-                        <div style={subtitle}>{`Avaliable: ${x.number}`}</div>
-                      </div>
-                      <div>
-                        <div style={{display: "flex", flexDirection: "row", marginBottom: "2%"}}>
-                          <div  style={{display: "flex", flexDirection: "column", alignItems: "flex-start", marginRight: "40%"}}>
-                            <div style={minitile}>Booth Size</div>
-                            <div style={maintext}>{`${x.size && x.size.length > 0 ? x.size[0] : "N/a"} ft. x ${x.size && x.size.length > 1 ? x.size[1] : "N/a"} ft.`}</div>
-                          </div>
-                          <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
-                            <div style={minitile}>Booth Price</div>
-                            <div style={maintext}>$25</div>
-                          </div>
+                {market.booths.map((x,i) => {
+                  var creating = this.props.reserve && !this.props.reserve.fetching && this.props.reserve.reserveData && this.checkUsed(x);
+                  return (
+                    <Booth key={i}>
+                        <div id="price" style={price}>{`$${Math.round(x.price)}`}</div>
+                        <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                          <div style={title}>{x.name}</div>
+                          <div style={{...subtitle, display: "flex"}}><div style={{marginRight: "10%"}}>{`Avaliable: `}</div> <div>{x.number < 0 ? <Spinner/> : <span>{x.number > -1 ? x.number : ""}</span>}</div></div>
                         </div>
-                        {x.description ? <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
-                          <div style={minitile}>Booth description</div>
-                          <div style={{textAlign: "left", ...maintext}}>{x.description}</div>
-                        </div> : null}
-                        <Button style={button}>Rent Booth</Button>
-                      </div>
-                      
-                  </Booth>
+                        <div>
+                          <div style={{display: "flex", flexDirection: "row", marginBottom: "2%"}}>
+                            <div  style={{display: "flex", flexDirection: "column", alignItems: "flex-start", marginRight: "40%"}}>
+                              <div style={minitile}>Booth Size</div>
+                              <div style={maintext}>{`${x.size && x.size.length > 0 ? x.size[0] : "N/a"} ft. x ${x.size && x.size.length > 1 ? x.size[1] : "N/a"} ft.`}</div>
+                            </div>
+                            <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                              <div style={minitile}>Booth Price</div>
+                              <div style={maintext}>$25</div>
+                            </div>
+                          </div>
+                          {x.description ? <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                            <div style={minitile}>Booth description</div>
+                            <div style={{textAlign: "left", ...maintext}}>{x.description}</div>
+                          </div> : null}
+                          {
+                              <Button style={this.props.reserve.fetching || this.disable ? buttonDisabled : creating ? button : buttonRed} onClick={()=>{this.disable = true; this.setState({...this.state}); creating ? this.props.createReservation(market.id, x.id, formatDate(this.state.date)) : this.fireDelete(x);}} disabled={this.props.reserve.fetching || this.disable}>{creating ? "Rent Booth" : "Delete Reservation"}</Button>
+                          }
+                        </div>
+                        
+                      </Booth>
+                    )
+                  }
                   )}
                   
-              </Expandor>
+              </Expandor> }
             </div>
         </div> 
 
     );
 }
+  checkUsed = (x) =>
+  {
+    
+    try{
+      return this.props.reserve.reserveData.filter(y => x.id === y.id && y.user_vdrs.length > 0).length == 0;
+    }catch {return false;}
+  }
+  fireDelete = (x)=>
+  {
+    if(!this.props.user || !this.props.user.userData) return;
+    console.log(x);
+    var ruser = this.props.user.userData.upcoming_vdr;
+    console.log(ruser);
+    if(!ruser && ruser.length < 1 ) return;
+    ruser = ruser.filter(y=> x.id === y.booth_id && x.market_id === y.market_id)
+    if(ruser.length < 1) return;
+    this.props.deleteBoothReservation(ruser[0].id, ruser[0].booth_id, ruser[0].market_id, formatDate(this.state.date));
+  } 
 }
 
+
+
+function formatDate(date)
+{
+  let d = date.getDate();
+  let m = date.getMonth()+1;
+  let y = date.getFullYear();
+  d = parseInt(d) < 10 ? `0${d}` : d;
+  m = parseInt(m) < 10 ? `0${m}` : m;
+  return `${y}-${m}-${d}`;
+}
+
+Date.prototype.isSameDateAs = function(pDate) {
+  return (
+    this.getFullYear() === pDate.getFullYear() &&
+    this.getMonth() === pDate.getMonth() &&
+    this.getDate() === pDate.getDate()
+  );
+}
+
+
+const ErrorDiv = styled.div`
+  margin 0 auto;
+  padding-left: 10px;
+  padding-top: 5px;
+  font-size: 18px;
+  font-family: Raleway;
+  color: #b21b2d;
+`
+
+const Spinner = styled.div`
+border: 2pt solid #aaa;
+border-radius: 50%;
+border-top: 2pt solid #478529;
+width: 8pt;
+height: 8pt;
+-webkit-animation: spin 2s linear infinite; /* Safari */
+animation: spin 2s linear infinite;
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+`
 const Booth = styled.div `
   display: flex;
   align-items: flex-end;
 `
-export default BoothPicker;
+const mapStateToProps = (state) =>
+{
+  return {
+    reserve: {...state.checkBoothReserve},
+    user: {...state.checkUserData}
+  }
+}
+export default connect(mapStateToProps, {createReservation, getBoothReservations, deleteBoothReservation, getUserData})(BoothPicker);
 
 
 /* {
