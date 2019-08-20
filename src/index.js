5bd55d5dc88fdc60e68c9633809a6cf42bbe1da6
AI2EJAY <39928154+AI2EJAY@users.noreply.github.com>
2019-08-20 16:10:59 -0500

    scroll fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/utls/store'
+import { BrowserRouter as Router } from "react-router-dom";
+import Scrollfix from "./components/Scrollfix";
+import { Provider } from "react-redux";
+import store from "./redux/utls/store";
 
-import './index.scss';
-import App from './App';
+import "./index.scss";
+import App from "./App";
 
-// const store = createStore(rootReducer, 
+// const store = createStore(rootReducer,
 //     applyMiddleware(thunk, logger));
-
 
 //withRouter(props => <App {...props} />); {/*allows top level to read the url tag */}
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App/>
-        </Router>
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <Router>
+      <Scrollfix>
+        <App />
+      </Scrollfix>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
