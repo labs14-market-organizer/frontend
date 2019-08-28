184ac1638e55bb80a2a93dd2c6d0a27a5b1478a9
AI2EJAY <39928154+AI2EJAY@users.noreply.github.com>
2019-08-28 13:12:58 -0500

    fixed double toggle from merge

--- src/components/NavbarMarket.js
+++ src/components/NavbarMarket.js
@@ -43,18 +43,6 @@ class NavbarMarket extends React.Component {
     }
   };
 
-  toggle = () => {
-    if (this.state.menuOpen === true) {
-      this.setState({
-        menuOpen: false
-      });
-    } else {
-      this.setState({
-        menuOpen: true
-      });
-    }
-  };
-
   Menu = () => {
     let usertype = this.props.user.userType;
     let avySwitch =
